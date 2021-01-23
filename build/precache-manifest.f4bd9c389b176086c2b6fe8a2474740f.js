fe813b7afd46e36635d9add6d3d6afcf130bf613
Takumi Masuda <takufrom1124@gmail.com>
2021-01-23 08:53:08 +0000

    build for pages

---
+++ build/precache-manifest.f4bd9c389b176086c2b6fe8a2474740f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3cecacf9d5877ff28f746657c93597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1804b1e9431ac7975806",
+    "url": "/static/css/2.e2f4d8cb.chunk.css"
+  },
+  {
+    "revision": "7b95f32e848783a80564",
+    "url": "/static/css/main.90ccf90b.chunk.css"
+  },
+  {
+    "revision": "1804b1e9431ac7975806",
+    "url": "/static/js/2.9bd83240.chunk.js"
+  },
+  {
+    "revision": "728dd6e28056bb9fff68dc25f7b229fb",
+    "url": "/static/js/2.9bd83240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b95f32e848783a80564",
+    "url": "/static/js/main.610a7f40.chunk.js"
+  },
+  {
+    "revision": "9cff5d353e1d72eadec8",
+    "url": "/static/js/runtime-main.bcda896a.js"
+  }
+]);
