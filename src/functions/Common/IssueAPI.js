a3405904d3666afa840d2b1667df658e4ff010bd
yhzz <20seikisyounen@gmail.com>
2021-02-02 13:35:38 +0000

    almost compete

--- src/functions/Common/IssueAPI.js
+++ src/functions/Common/IssueAPI.js
@@ -1,5 +1,5 @@
 import { isGitHubURL } from '../GitHub/GitHubURLHelper.js';
-import { isGitLabURL } from '../GitLab/GitLabURLHelper.js';
+import { isGitLabURL, getSelfHostingGitLabDomain } from '../GitLab/GitLabURLHelper.js';
 import {
   getGitHubIssuesFromAPI,
   updateGitHubIssueFromGanttTask,
@@ -16,11 +16,11 @@ import {
 } from '../GitLab/GitLabAPI.js';
 
 export const getIssuesFromAPI = async (gantt, git_url, token, selected_labels) => {
-  gantt.clearAll(); 
+  gantt.clearAll();
   if (isGitHubURL(git_url)) {
     getGitHubIssuesFromAPI(gantt, git_url, selected_labels);
   }
-  if (isGitLabURL(git_url)) {
+  if (isGitLabURL(git_url) || getSelfHostingGitLabDomain(git_url) !== null) {
     getGitLabIssuesFromAPI(gantt, git_url, token, selected_labels);
   }
 }
@@ -29,7 +29,7 @@ export const setLabelListOfRepoFromAPI = async (_this, git_url, token) => {
   if (isGitHubURL(git_url)) {
     setGitHubLabelListOfRepoFromAPI(_this, git_url);
   }
-  if (isGitLabURL(git_url)) {
+  if (isGitLabURL(git_url) || getSelfHostingGitLabDomain(git_url) !== null) {
     setGitLabLabelListOfRepoFromAPI(_this, git_url, token);
   }
 }
@@ -38,7 +38,7 @@ export const updateIssueByAPI = (gantt_task_id, token, gantt, git_url) => {
   if (isGitHubURL(git_url)) {
     updateGitHubIssueFromGanttTask(gantt_task_id, token, gantt, git_url);
   }
-  if (isGitLabURL(git_url)) {
+  if (isGitLabURL(git_url) || getSelfHostingGitLabDomain(git_url) !== null) {
     updateGitLabIssueFromGanttTask(gantt_task_id, token, gantt, git_url);
   }
 }
@@ -47,7 +47,7 @@ export const openIssueAtBrowser = (gantt_task_id, git_url) => {
   if (isGitHubURL(git_url)) {
     openGitHubIssueAtBrowser(gantt_task_id, git_url);
   }
-  if (isGitLabURL(git_url)) {
+  if (isGitLabURL(git_url) || getSelfHostingGitLabDomain(git_url) !== null) {
     openGitLabIssueAtBrowser(gantt_task_id, git_url);
   }
 };
@@ -56,7 +56,7 @@ export const openNewIssueAtBrowser = (gantt_task_id, git_url) => {
   if (isGitHubURL(git_url)) {
     openGitHubNewIssueAtBrowser(gantt_task_id, git_url);
   }
-  if (isGitLabURL(git_url)) {
+  if (isGitLabURL(git_url) || getSelfHostingGitLabDomain(git_url) !== null) {
     openGitLabNewIssueAtBrowser(gantt_task_id, git_url);
   }
 }
