0a8e7bbf9a1e975409b6d87f78f04369c02f5465
yhzz <20seikisyounen@gmail.com>
2021-02-06 07:56:39 +0000

    conflit2

--- src/functions/GitLab/GitLabURLHelper.js
+++ src/functions/GitLab/GitLabURLHelper.js
@@ -1,8 +1,5 @@
 import { adjustURL } from '../Common/Parser.js';
-<<<<<<< HEAD
 import { isValidVariable } from '../Common/CommonHelper.js';
-=======
->>>>>>> origin/main
 
 export const isGitLabURL = (git_url) => {
   return /gitlab\.com/.test(git_url);
