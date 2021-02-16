7865b1f478e94e39c830c38baf7e07ddc4251abe
Yohei Zaiznen <20seikisyounen@gmail.com>
2021-02-17 01:10:48 +0900

    test

--- src/functions/GitHub/GitHubAPI.test.js
+++ src/functions/GitHub/GitHubAPI.test.js
@@ -21,3 +21,13 @@ jest.mock('axios');
 //     });
 //   });
 // });
+
+describe('true is truthy and false is falsy', () => {
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+
+  test('false is falsy', () => {
+    expect(false).toBe(false);
+  });
+});
