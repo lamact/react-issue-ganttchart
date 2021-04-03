233754ce10aaa60c9285b8596cfb54ccab7b6088
FujitaYukiya <data.jtky@gmail.com>
2021-04-03 16:57:38 +0900

    4/3  3

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,78 +6,65 @@ import { isValidVariable } from '../../functions/Common/CommonHelper.js';
 
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-    },
-  ];
-  
-  const rows = [
-    { id: "#1", lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-
-  let custom_data=[];
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+  },
+];
+let custom_columns = [];
+let custom_data = [];
 
 const Table = (props) => {
-    const { classes } = props;
-    useEffect(() => {
-      getIssuesFromAPI(
-        props.git_url,
-        props.token,
-        props.selected_labels,
-        props.selected_assignee
-      )
-        .then((issuedata) => {
-          if (isValidVariable(issuedata)) {
-            custom_data=issuedata;
-
-          }
-        })
-        .catch((err) => {
-          console.log( 'error' );
-        });
-    }, [
+  const { classes } = props;
+  useEffect(() => {
+    getIssuesFromAPI(
       props.git_url,
       props.token,
       props.selected_labels,
-      props.selected_assignee,
-      props.update,
-      
-    ]);
+      props.selected_assignee
+    )
+      .then((issuedata) => {
+        if (isValidVariable(issuedata)) {
+          custom_data = issuedata;
+        }
+      })
+      .catch((err) => {
+        console.log('error');
+      });
+  }, [
+    props.git_url,
+    props.token,
+    props.selected_labels,
+    props.selected_assignee,
+    props.update,
 
-    //console.log(" before datanum : "+datas.length);
-    props.TableupdateIssueByAPI();
+  ]);
 
-  
+  //console.log(" before datanum : "+datas.length);
+  props.TableupdateIssueByAPI();
+
+
 
 
 
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={custom_data} columns={columns} pageSize={5} checkboxSelection />
+      <DataGrid rows={custom_data} columns={custom_columns} pageSize={5} checkboxSelection />
     </div>
   );
 };
