cab25bf49870f2bb0fe5250f5fd392868895489d
FujitaYukiya <data.jtky@gmail.com>
2021-04-11 15:21:04 +0900

    reacttable start

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,23 +1,200 @@
-import React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+// import React from 'react';
+// import { DataGrid } from '@material-ui/data-grid';
 
-const Table = (props) => {
+// const Table = (props) => {
+//   return (
+//     <div style={{ height: 400, width: '100%' }}>
+//       {props.issue ? (
+//       <DataGrid
+//         rows={props.issue}
+//         columns={props.issue_columns}
+//         pageSize={5}
+//         checkboxSelection
+//         />
+//       ) : (
+//         <div>
+//         urlを入力ください
+//       </div>
+//     )} 
+//     </div>
+//   );
+// };
+
+// export default Table;
+
+
+import React from 'react'
+import styled from 'styled-components'
+import { useTable, useSortBy  } from 'react-table'
+
+import makeData from './makeData'
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef()
+    const resolvedRef = ref || defaultRef
+
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate
+    }, [resolvedRef, indeterminate])
+
+    return <input type="checkbox" ref={resolvedRef} {...rest} />
+  }
+)
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    allColumns,
+    getToggleHideAllColumnsProps,
+    state,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  )
+
+  // Render the UI for your table
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      {props.issue ? (
-      <DataGrid
-        rows={props.issue}
-        columns={props.issue_columns}
-        pageSize={5}
-        checkboxSelection
-        />
-      ) : (
+    <>
+      <div>
         <div>
-        urlを入力ください
+          <IndeterminateCheckbox {...getToggleHideAllColumnsProps()} /> Toggle
+          All
+        </div>
+        {allColumns.map(column => (
+          <div key={column.id}>
+            <label>
+              <input type="checkbox" {...column.getToggleHiddenProps()} />{' '}
+              {column.id}
+            </label>
+          </div>
+        ))}
+        <br />
       </div>
-    )} 
-    </div>
-  );
-};
+      <table {...getTableProps()}>
+        <thead>
+        {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </>
+  )
+}
 
-export default Table;
+function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = React.useMemo(() => makeData(20), [])
+
+  return (
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
+  )
+}
+
+export default App
